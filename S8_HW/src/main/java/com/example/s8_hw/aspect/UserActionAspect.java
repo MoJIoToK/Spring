ce7d50aa5591709e4cbaba165f09a8b75ce5c47c
Nick <mojiotok167@gmail.com>
2024-02-08 20:01:23 +0300

    hw 8 seminar - done

--- S8_HW/src/main/java/com/example/s8_hw/aspect/UserActionAspect.java
+++ S8_HW/src/main/java/com/example/s8_hw/aspect/UserActionAspect.java
@@ -1,32 +1,38 @@
 package com.example.s8_hw.aspect;
 
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
-import javax.xml.stream.events.Comment;
 import java.util.Arrays;
-import java.util.Objects;
 
 @Aspect
+@Component
+@RequiredArgsConstructor
 public class UserActionAspect {
 
-    @Around("execution(* org.example.service.*.*(..))")
-    public Object  trackUserAction(ProceedingJoinPoint joinPoint) throws Throwable{
+    @Around("@annotation(com.example.s8_hw.aspect.TrackUserAction)")
+    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().getName();
         Object [] arguments = joinPoint.getArgs();
 
-        System.out.println("Method " + methodName +
-                " with parameters " + Arrays.asList(arguments) +
-                " will execute.");
+        System.out.println("Метод вызванный пользователем - " + methodName +
+                ", с параметрами " + Arrays.asList(arguments) +
+                " был завершен.");
 
         long start = System.currentTimeMillis();
         Object result = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
-        System.out.println("Сообщение до вызова комментируемого метода");
-
+        System.out.println("Метод - " + methodName +
+                ", выполнился за " + executionTime +
+                ", с результатом -" + result);
+//        System.out.println("Пользователь вызвал метод: " +
+//                joinPoint.getSignature().getName() + ".");
+//        Object proceed = joinPoint.proceed();
+//        System.out.println("Метод завершён.");
         return result;
-
     }
 
 }
